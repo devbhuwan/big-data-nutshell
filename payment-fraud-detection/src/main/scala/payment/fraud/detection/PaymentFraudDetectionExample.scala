5b825e2a9ad15d26e0523b7ed43eaf4cc65e1c96
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-09-03 14:42:06 +0400

    BIG DATA NUTSHELL

--- payment-fraud-detection/src/main/scala/payment/fraud/detection/PaymentFraudDetectionExample.scala
+++ payment-fraud-detection/src/main/scala/payment/fraud/detection/PaymentFraudDetectionExample.scala
@@ -18,7 +18,7 @@ object PaymentFraudDetectionExample {
     StructField("cardNo", StringType, nullable = true),
     StructField("location", StringType, nullable = true),
     StructField("amount", StringType, nullable = true),
-    StructField("time", DateType, nullable = true)
+    StructField("time", StringType, nullable = true)
   ))
 
   def main(args: Array[String]): Unit = {
@@ -27,14 +27,23 @@ object PaymentFraudDetectionExample {
       .getOrCreate()
 
     import spark.implicits._
-    val train: Dataset[Payment] = spark.read.option("inferSchema", "false")
+    val train: Dataset[Payment] = spark.read.option("paymentSchema", "false")
       .schema(schema).csv("payment-fraud-detection/data/payment.csv").as[Payment]
     train.take(1)
     train.cache
     println(train.count)
 
+    val test: Dataset[Payment] = spark.read.option("paymentSchema", "false")
+      .schema(schema).csv("payment-fraud-detection/data/payment.csv").as[Payment]
+    test.take(2)
+    println(test.count)
+    test.cache
+
+    train.printSchema()
+    train.show
+
   }
 
-  case class Payment(id: String, customerName: String, cardNo: String, location: String, amount: BigDecimal, time: Date)
+  case class Payment(id: String, customerName: String, cardNo: String, location: String, amount: String, time: String)
 
 }
